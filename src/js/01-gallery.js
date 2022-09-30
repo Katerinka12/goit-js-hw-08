b62cca92cdd9708fc3d8461fcf7d87d5aab36fd1
Katerinka12 <katerinka.kk12@gmail.com>
2022-09-30 12:36:22 +0300

    add simplelight

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,9 @@
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
 console.log(galleryItems);
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
